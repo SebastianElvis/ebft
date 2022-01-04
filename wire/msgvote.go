89de5b4cb64631edae0fcd90b9eb18f159693854
Runchao Han <runchao.han@monash.edu>
2022-01-04 21:48:56 +1100

    msgvote

--- wire/msgvote.go
+++ wire/msgvote.go
@@ -1,10 +1,44 @@
 package wire
 
-import "github.com/btcsuite/btcd/chaincfg/chainhash"
+import (
+	"io"
+
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"golang.org/x/crypto/ripemd160"
+)
 
 type MsgVote struct {
 	Address        string
 	VotedBlockHash chainhash.Hash
 }
 
-// TODO: see https://github.com/SebastianElvis/orazor/issues/3
+// TODO: see https://github.com/SebastianElvis/orazor/issues/8
+
+// BtcEncode encodes the receiver to w using the bitcoin protocol encoding.
+// This is part of the Message interface implementation.
+func (msg *MsgVote) BtcEncode(w io.Writer, pver uint32, enc MessageEncoding) error {
+	return writeElements(w, msg.Address, &msg.VotedBlockHash)
+}
+
+// BtcDecode decodes r using the bitcoin protocol encoding into the receiver.
+func (msg *MsgVote) BtcDecode(r io.Reader, pver uint32, enc MessageEncoding) error {
+	return readElements(r, msg.Address, &msg.VotedBlockHash)
+}
+
+// Command returns the protocol command string for the message.  This is part
+// of the Message interface implementation.
+func (msg *MsgVote) Command() string {
+	return CmdVote
+}
+
+// MaxPayloadLength returns the maximum length the payload can be for the
+// receiver.  This is part of the Message interface implementation.
+func (msg *MsgVote) MaxPayloadLength(pver uint32) uint32 {
+	return 1 + ripemd160.Size + chainhash.HashSize
+}
+
+// MsgVote returns a new message that conforms to the Message
+// interface.  See MsgVote for details.
+func NewMsgVote() *MsgVote {
+	return &MsgVote{}
+}
