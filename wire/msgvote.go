536361a68ec8df718d0e3ebe882f89f69e000e2f
Runchao Han <runchao.han@monash.edu>
2022-01-04 22:08:44 +1100

    type of votes

--- wire/msgvote.go
+++ wire/msgvote.go
@@ -7,9 +7,17 @@ import (
 	"golang.org/x/crypto/ripemd160"
 )
 
+type VoteType uint8
+
+const (
+	VTCertify VoteType = iota
+	VTUniqueAnnounce
+)
+
 type MsgVote struct {
 	Address        string
 	VotedBlockHash chainhash.Hash
+	Type           VoteType
 }
 
 // TODO: see https://github.com/SebastianElvis/orazor/issues/8
@@ -17,12 +25,12 @@ type MsgVote struct {
 // BtcEncode encodes the receiver to w using the bitcoin protocol encoding.
 // This is part of the Message interface implementation.
 func (msg *MsgVote) BtcEncode(w io.Writer, pver uint32, enc MessageEncoding) error {
-	return writeElements(w, msg.Address, &msg.VotedBlockHash)
+	return writeElements(w, msg.Address, &msg.VotedBlockHash, msg.Type)
 }
 
 // BtcDecode decodes r using the bitcoin protocol encoding into the receiver.
 func (msg *MsgVote) BtcDecode(r io.Reader, pver uint32, enc MessageEncoding) error {
-	return readElements(r, msg.Address, &msg.VotedBlockHash)
+	return readElements(r, msg.Address, &msg.VotedBlockHash, msg.Type)
 }
 
 // Command returns the protocol command string for the message.  This is part
@@ -34,7 +42,7 @@ func (msg *MsgVote) Command() string {
 // MaxPayloadLength returns the maximum length the payload can be for the
 // receiver.  This is part of the Message interface implementation.
 func (msg *MsgVote) MaxPayloadLength(pver uint32) uint32 {
-	return 1 + ripemd160.Size + chainhash.HashSize
+	return 1 + ripemd160.Size + chainhash.HashSize + 1
 }
 
 // MsgVote returns a new message that conforms to the Message
