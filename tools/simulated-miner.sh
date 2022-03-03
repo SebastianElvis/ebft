91b974d19cab2b29ba1d1f0b9aa2f4904112b232
Runchao Han <runchao.han@monash.edu>
2022-03-03 13:50:48 +1100

    Squashed commit of the following:
    commit 5162c654f211d4374c0637bef08e714e708efc58
    Author: Runchao Han <runchao.han@monash.edu>
    Date:   Thu Mar 3 13:49:31 2022 +1100
    
        generatetoaddress
    
    commit b3036faa9e847b3a9bf76013e59a63c3cc5387ad
    Author: Runchao Han <runchao.han@monash.edu>
    Date:   Wed Mar 2 17:05:10 2022 +1100
    
        minor
    
    commit 84e8c01de2d80b0cd38275a7def818332781ca9e
    Author: Runchao Han <runchao.han@monash.edu>
    Date:   Wed Mar 2 16:53:52 2022 +1100
    
        docker compose script
    
    commit e99dd449c1300a7d8209b032fb9066c218922c9e
    Author: Runchao Han <runchao.han@monash.edu>
    Date:   Wed Mar 2 13:52:28 2022 +1100
    
        dockerfile
    
    commit 916f8f4508bb919588a3b343a6162c5d3da4fbb3
    Author: Runchao Han <runchao.han@monash.edu>
    Date:   Wed Mar 2 11:27:45 2022 +1100
    
        fix
    
    commit 9f1cb7eb1f7d426731bfde115d107e2a13b49a91
    Author: Runchao Han <runchao.han@monash.edu>
    Date:   Sun Feb 27 15:19:20 2022 +1100
    
        wallet data and info
    
    commit 19d24524172ec33d6236bb7a3424447ef6ed7663
    Merge: b398e7e 7016811
    Author: Runchao Han <runchao.han@monash.edu>
    Date:   Sun Feb 27 14:58:05 2022 +1100
    
        Merge branch 'main' into local-test-env
    
    commit b398e7ec060b0f79feebcdd9d2486b606b49252f
    Author: Runchao Han <runchao.han@monash.edu>
    Date:   Sun Feb 27 12:37:18 2022 +1100
    
        minor
    
    commit 618cef42243170ec810ad26b1fb138c5e0cbba44
    Author: Runchao Han <runchao.han@monash.edu>
    Date:   Sat Feb 19 15:12:33 2022 +1100
    
        reorg

--- tools/simulated-miner.sh
+++ tools/simulated-miner.sh
@@ -1,12 +1,19 @@
-num_blocks=$1
-interval=$2
+endpoint=$1
+num_blocks=$2
+interval=$3
+committee_size=$4
+
+# read address list as $x
+mapfile -t x < address-list.txt
 
 echo "$num_blocks blocks with interval $interval will be generated"
 
 for i in $(seq $num_blocks)
 do 
-    echo "Generating block #$i"
-    btcctl --simnet --rpcuser=USER --rpcpass=PASS generate 1
+    r=$((1 + $RANDOM % $committee_size))
+    addr=${x[$r]}
+    btcctl --simnet --rpcserver=$1 --rpcuser=USER --rpcpass=PASS generatetoaddress 1 $addr
+    echo "Generated block #$i to address $addr"
     i=$((i+1))
-    sleep $2
+    sleep $3
 done
