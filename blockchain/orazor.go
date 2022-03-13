3d0d5180d1943633a83fab0f54740f1be77d606d
Runchao Han <runchao.han@monash.edu>
2022-03-14 09:53:41 +1100

    minor

--- blockchain/orazor.go
+++ blockchain/orazor.go
@@ -7,15 +7,9 @@ import (
 	"github.com/btcsuite/btcd/wire"
 )
 
+// ProcessVote processes a vote message
+// returns (newlyCertified, duplicatedVote, error)
 func (b *BlockChain) ProcessVote(vote *wire.MsgVote) (bool, bool, error) {
-	if b.chainParams.Extension != chaincfg.ExtSyncORazor && b.chainParams.Extension != chaincfg.ExtPSyncORazor {
-		return false, false, fmt.Errorf("vote message cam only exist with extension ExtSyncORazor or ExtPSyncORazor")
-	}
-
-	votedBlockHash := vote.VotedBlockHash
-	voteType := vote.Type
-	addr := string(vote.Address[:])
-
 	// signature := vote.Signature
 	// // verify signature
 	// pk, _, err := btcec.RecoverCompact(btcec.S256(), signature, votedBlockHash[:])
@@ -29,6 +23,40 @@ func (b *BlockChain) ProcessVote(vote *wire.MsgVote) (bool, bool, error) {
 	// 	return err
 	// }
 
+	// get the committee
+	committee, err := b.Committee()
+	if err != nil {
+		return false, false, err
+	}
+	// the voter should be a part of the committee
+	addr := string(vote.Address[:])
+	if _, ok := committee[addr]; !ok {
+		return false, false, fmt.Errorf("voter %v is not a part of the committee", addr)
+	}
+
+	if b.chainParams.Extension == chaincfg.ExtSyncORazor {
+		return b.syncProcessVote(vote)
+	} else if b.chainParams.Extension == chaincfg.ExtPSyncORazor {
+		return b.psyncProcessVote(vote)
+	} else {
+		return false, false, fmt.Errorf("vote message cam only exist with extension ExtSyncORazor or ExtPSyncORazor")
+	}
+}
+
+// ProcessVote processes a vote message
+// returns (newlyCertified, duplicatedVote, error)
+func (b *BlockChain) syncProcessVote(vote *wire.MsgVote) (bool, bool, error) {
+	votedBlockHash := vote.VotedBlockHash
+	voteType := vote.Type
+	addr := string(vote.Address[:])
+
+	// SyncORazor does not have UniqueAnnounce
+	if voteType != wire.VTCertify {
+		return false, false, fmt.Errorf("wrong vote type in SyncORazor-simnet: %s", voteType.String())
+	}
+
+	// quorum
+	quorum := b.chainParams.CommitteeSize*1/2 + 1
 	committee, err := b.Committee()
 	if err != nil {
 		return false, false, err
@@ -38,23 +66,83 @@ func (b *BlockChain) ProcessVote(vote *wire.MsgVote) (bool, bool, error) {
 		return false, false, fmt.Errorf("voter %v is not a part of the committee", addr)
 	}
 
-	if b.chainParams.Extension == chaincfg.ExtSyncORazor {
-		// quorum
-		quorum := b.chainParams.CommitteeSize*1/2 + 1
-		// SyncORazor does not have UniqueAnnounce
-		if voteType != wire.VTCertify {
-			return false, false, fmt.Errorf("wrong vote type in SyncORazor-simnet: %s", voteType.String())
+	// get the block
+	block, err := b.BlockByHashNoMainChain(&votedBlockHash)
+	if err != nil {
+		return false, false, fmt.Errorf("voted block %v does not exist: %v", votedBlockHash, err)
+	}
+	// get the blockNode
+	blockNode := b.index.LookupNode(&votedBlockHash)
+	// if the block is already certified or finalised, return (false, false, nil)
+	if blockNode.status.KnownCertified() {
+		return false, false, nil
+	}
+
+	// add vote
+	if _, ok := blockNode.certifyVotes[addr]; !ok {
+		blockNode.certifyVotes[addr] = vote
+	} else {
+		// duplicated vote, skip
+		return false, true, nil
+	}
+
+	// if **newly** meet the certify requirement,
+	//   - certify the block
+	//   - refresh the best chain
+	//   - refresh the committee
+	if totalCertifyVotes(blockNode, committee) == quorum {
+		// certify the block
+		b.index.SetStatusFlags(blockNode, statusCertified)
+		// change the bestchain
+		b.chainLock.Lock()
+		if _, err := b.connectBestChain(blockNode, block, BFNone); err != nil {
+			return false, false, err
 		}
-		// get the block
-		block, err := b.BlockByHashNoMainChain(&votedBlockHash)
+		b.chainLock.Unlock()
+		log.Infof("extension SyncORazor: block %v has been certified", blockNode.hash)
+		// refresh the committee
+		b.committeeAddrs, err = b.Committee()
 		if err != nil {
-			return false, false, fmt.Errorf("voted block %v does not exist: %v", votedBlockHash, err)
+			return false, false, fmt.Errorf("refresh committee upon new block %v", votedBlockHash)
 		}
-		// get the blockNode
-		blockNode := b.index.LookupNode(&votedBlockHash)
-		// if the block is already certified or finalised, return nil directly
+		return true, false, nil
+	} else {
+		return false, false, nil
+	}
+}
+
+// ProcessVote processes a vote message
+// returns (newlyCertified, duplicatedVote, error)
+func (b *BlockChain) psyncProcessVote(vote *wire.MsgVote) (bool, bool, error) {
+	votedBlockHash := vote.VotedBlockHash
+	voteType := vote.Type
+	addr := string(vote.Address[:])
+
+	// quorum size
+	quorum := b.chainParams.CommitteeSize*2/3 + 1
+	// get the committee
+	committee, err := b.Committee()
+	if err != nil {
+		return false, false, err
+	}
+	// the voter should be a part of the committee
+	if _, ok := committee[addr]; !ok {
+		return false, false, fmt.Errorf("voter %v is not a part of the committee", addr)
+	}
+
+	// get the block
+	block, err := b.BlockByHashNoMainChain(&votedBlockHash)
+	if err != nil {
+		return false, false, fmt.Errorf("block %v does not exist: %v", votedBlockHash, err)
+	}
+	// get the blockNode
+	blockNode := b.index.LookupNode(&votedBlockHash)
+
+	// this is a certify vote
+	if voteType == wire.VTCertify {
+		// if the block is already certified or finalised, return (false, false, nil)
 		if blockNode.status.KnownCertified() {
-			return false, false, fmt.Errorf("block %v is already certified or finalised", votedBlockHash)
+			return false, false, nil
 		}
 		// add vote
 		if _, ok := blockNode.certifyVotes[addr]; !ok {
@@ -63,14 +151,11 @@ func (b *BlockChain) ProcessVote(vote *wire.MsgVote) (bool, bool, error) {
 			// duplicated vote, skip
 			return false, true, nil
 		}
-		// if meet the certify requirement, certify the block
-		var totalVotes uint32 = 0
-		for k := range blockNode.certifyVotes {
-			if v, ok := committee[k]; ok {
-				totalVotes += v
-			}
-		}
-		if totalVotes >= quorum {
+		// if **newly** meet the certify requirement
+		//   - certify the block
+		//   - refresh the best chain
+		//   - refresh the committee
+		if totalCertifyVotes(blockNode, committee) == quorum {
 			// certify the block
 			b.index.SetStatusFlags(blockNode, statusCertified)
 			// change the bestchain
@@ -79,94 +164,68 @@ func (b *BlockChain) ProcessVote(vote *wire.MsgVote) (bool, bool, error) {
 				return false, false, err
 			}
 			b.chainLock.Unlock()
-			log.Infof("extension SyncORazor: block %v has been certified", blockNode.hash)
-			// refresh the committee
-			b.committeeAddrs, err = b.Committee()
-			if err != nil {
-				return false, false, fmt.Errorf("refresh committee upon new block %v", votedBlockHash)
-			}
+			log.Infof("extension PSyncORazor: block %v has been certified", blockNode.hash)
 			return true, false, nil
 		} else {
 			return false, false, nil
 		}
-	} else if b.chainParams.Extension == chaincfg.ExtPSyncORazor {
-		// quorum size
-		quorum := b.chainParams.CommitteeSize*2/3 + 1
-		// get the block
-		block, err := b.BlockByHash(&votedBlockHash)
-		if err != nil {
-			return false, false, fmt.Errorf("block %v does not exist: %v", votedBlockHash, err)
-		}
-		// get the blockNode
-		blockNode := b.index.LookupNode(&votedBlockHash)
-
-		if voteType == wire.VTCertify {
-			// if the block is already certified or finalised, return nil directly
-			if blockNode.status.KnownCertified() {
-				return false, false, fmt.Errorf("block %v is already certified or finalised", votedBlockHash)
-			}
-			// add vote
-			if _, ok := blockNode.certifyVotes[addr]; !ok {
-				blockNode.certifyVotes[addr] = vote
-			} else {
-				return false, true, nil
-			}
-			// if meet the certify requirement, certify the block
-			var totalVotes uint32 = 0
-			for k := range blockNode.certifyVotes {
-				if v, ok := committee[k]; ok {
-					totalVotes += v
-				}
-			}
-			if totalVotes >= quorum {
-				// certify the block
-				b.index.SetStatusFlags(blockNode, statusCertified)
-				// change the bestchain
-				b.chainLock.Lock()
-				if _, err := b.connectBestChain(blockNode, block, BFNone); err != nil {
-					return false, false, err
-				}
-				b.chainLock.Unlock()
-				log.Infof("extension PSyncORazor: block %v has been certified", blockNode.hash)
-				return true, false, nil
-			} else {
-				return false, false, nil
-			}
-		} else if voteType == wire.VTUniqueAnnounce {
-			// add vote
-			if _, ok := blockNode.uniqueAnnounceVotes[addr]; !ok {
-				blockNode.uniqueAnnounceVotes[addr] = vote
-			} else {
-				return false, true, nil
-			}
-			// if meet the finalisation requirement, finalise all blocks before this block
-			var totalVotes uint32 = 0
-			for k := range blockNode.uniqueAnnounceVotes {
-				if v, ok := committee[k]; ok {
-					totalVotes += v
-				}
-			}
-			if totalVotes >= quorum && blockNode.status.KnownCertified() {
-				// finalise the block and its ancestors
-				curBlock := blockNode
-				for {
-					if !curBlock.status.KnownFinalized() {
-						b.index.SetStatusFlags(blockNode, statusFinalized)
-						log.Infof("extension PSyncORazor: block %v has been finalised", curBlock.hash)
-						if curBlock.parent == nil {
-							break
-						} else {
-							curBlock = curBlock.parent
-						}
-					} else {
-						break
-					}
-				}
-				return true, false, nil
-			}
-		}
-	} else {
-		return false, false, fmt.Errorf("wrong network: %s", b.chainParams.Name)
 	}
-	return false, false, nil
+
+	// this is a unique announce vote
+	if voteType == wire.VTUniqueAnnounce {
+		// the voted block should be certified
+		if !blockNode.status.KnownCertified() {
+			return false, false, fmt.Errorf("the UA-voted block %v is not certified yet", blockNode.hash)
+		}
+
+		// add vote
+		if _, ok := blockNode.uniqueAnnounceVotes[addr]; !ok {
+			blockNode.uniqueAnnounceVotes[addr] = vote
+		} else {
+			// duplicated vote
+			return false, true, nil
+		}
+		// if **newly** meet the finalisation requirement
+		//   - finalise the block and its ancestors
+		if totalUAVotes(blockNode, committee) == quorum {
+			curBlock := blockNode
+			for {
+				if !curBlock.status.KnownFinalized() {
+					b.index.SetStatusFlags(blockNode, statusFinalized)
+					log.Infof("extension PSyncORazor: block %v has been finalised", curBlock.hash)
+					if curBlock.parent == nil {
+						break
+					} else {
+						curBlock = curBlock.parent
+					}
+				} else {
+					break
+				}
+			}
+			return true, false, nil
+		}
+	}
+	return false, false, fmt.Errorf("unknown vote type: %s", voteType.String())
+}
+
+func totalCertifyVotes(node *blockNode, committee map[string]uint32) uint32 {
+	// if meet the certify requirement, certify the block
+	var totalVotes uint32 = 0
+	for k := range node.certifyVotes {
+		if v, ok := committee[k]; ok {
+			totalVotes += v
+		}
+	}
+	return totalVotes
+}
+
+func totalUAVotes(node *blockNode, committee map[string]uint32) uint32 {
+	// if meet the certify requirement, certify the block
+	var totalVotes uint32 = 0
+	for k := range node.uniqueAnnounceVotes {
+		if v, ok := committee[k]; ok {
+			totalVotes += v
+		}
+	}
+	return totalVotes
 }
